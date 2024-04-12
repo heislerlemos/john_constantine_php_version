d02e08e1161cb01ce1e0ff6e3a00c46a54c3d599
heislerlemos <heisler.stlano@gmail.com>
2024-04-12 16:52:29 +0100

    adicionou-se multiplos utilizadores

--- app/Http/Controllers/Centros_De_FormacoesController.php
+++ app/Http/Controllers/Centros_De_FormacoesController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Centro_de_formacao;
+use App\Http\Controllers\EdicaoController;
 use Illuminate\Http\Request;
 
 class Centros_De_FormacoesController extends Controller
 {
+
+
 
     /**
      * Display a listing of the resource.
@@ -63,9 +66,12 @@ class Centros_De_FormacoesController extends Controller
 
     public function edit(Centro_de_formacao $centros_de_formaco)
     {
-        echo url()->current();
-        
-        if (url()->current() == 'http://localhost:8080/centros_de_formacoes/10/edit') {
+  /**       echo url()->current();
+        $estudantes = (new EdicaoController);  
+        $estudantes->estudanteid();
+    **/
+
+           if (url()->current() == 'http://localhost:8080/centros_de_formacoes/1/edit') {
 
         if (!isset($_SERVER['PHP_AUTH_USER'])) {
             header('WWW-Authenticate: Basic realm="My Website"');
@@ -75,7 +81,7 @@ class Centros_De_FormacoesController extends Controller
         }
 
             
-        if ($_SERVER['PHP_AUTH_PW'] == 'ulapass' && $_SERVER['PHP_AUTH_USER'] == 'ula') {
+        if ($_SERVER['PHP_AUTH_PW'] == 'ula' && $_SERVER['PHP_AUTH_USER'] == 'ula') {
             echo '<p>Access granted. You know the password!</p>';
         return view('centros_de_formacoes.edit',
             compact('centros_de_formaco'));
@@ -83,6 +89,25 @@ class Centros_De_FormacoesController extends Controller
             echo '<p>Access denied! You do not know the password.</p>';
         } 
       }
+
+            if (url()->current() == 'http://localhost:8080/centros_de_formacoes/2/edit') {
+
+        if (!isset($_SERVER['PHP_AUTH_USER'])) {
+            header('WWW-Authenticate: Basic realm="My Website"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo '<p>Access denied. You did not enter a password.</p>';
+            exit; 
+        }
+
+            
+        if ($_SERVER['PHP_AUTH_PW'] == 'ugs' && $_SERVER['PHP_AUTH_USER'] == 'ugs') {
+            echo '<p>Access granted. You know the password!</p>';
+        return view('centros_de_formacoes.edit',
+            compact('centros_de_formaco'));
+        } else {
+            echo '<p>Access denied! You do not know the password.</p>';
+        } 
+      } 
     }
 
 
---
+++ app/Http/Controllers/EdicaoController.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace App\Http\Controllers;
+use App\Models\Centro_de_formacao;
+use Illuminate\Http\Request;
+
+
+class EdicaoController extends Controller
+{
+
+public function estudanteid() {
+
+        if (url()->current() == 'http://localhost:8080/centros_de_formacoes/1/edit') {
+
+        if (!isset($_SERVER['PHP_AUTH_USER'])) {
+            header('WWW-Authenticate: Basic realm="My Website"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo '<p>Access denied. You did not enter a password.</p>';
+            exit; 
+        }
+
+            
+        if ($_SERVER['PHP_AUTH_PW'] == 'ula' && $_SERVER['PHP_AUTH_USER'] == 'ula') {
+            echo '<p>Access granted. You know the password!</p>';
+        return view('centros_de_formacoes.edit',
+            compact('centros_de_formaco'));
+        } else {
+            echo '<p>Access denied! You do not know the password.</p>';
+        } 
+      }
+
+
+
+
+         if (url()->current() == 'http://localhost:8080/centros_de_formacoes/2/edit') {
+
+        if (!isset($_SERVER['PHP_AUTH_USER'])) {
+            header('WWW-Authenticate: Basic realm="My Website"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo '<p>Access denied. You did not enter a password.</p>';
+            exit; 
+        }
+
+            
+        if ($_SERVER['PHP_AUTH_PW'] == 'ugs' && $_SERVER['PHP_AUTH_USER'] == 'ugs') {
+            echo '<p>Access granted. You know the password!</p>';
+        return view('centros_de_formacoes.edit',
+            compact('centros_de_formaco'));
+        } else {
+            echo '<p>Access denied! You do not know the password.</p>';
+        } 
+      }
+}
+}
