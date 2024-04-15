9032dbe907ea4ab4bb44cd2dc33f727b15970140
heislerlemos <heisler.stlano@gmail.com>
2024-04-15 15:38:20 +0100

    adiciondo redirecionamento de authenticação

--- app/Http/Controllers/Centros_De_FormacoesController.php
+++ app/Http/Controllers/Centros_De_FormacoesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Centro_de_formacao;
 use App\Http\Controllers\EdicaoController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class Centros_De_FormacoesController extends Controller
 {
@@ -60,65 +61,62 @@ class Centros_De_FormacoesController extends Controller
 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
 
     public function edit(Centro_de_formacao $centros_de_formaco)
     {
-  /**       echo url()->current();
+  /**   
+        Trabalhando na refaturação do codigo.... 
+        echo url()->current();
         $estudantes = (new EdicaoController);  
         $estudantes->estudanteid();
-    **/
+  **/
 
-           if (url()->current() == 'http://localhost:8080/centros_de_formacoes/1/edit') {
+        if (url()->current() == 'http://localhost:8080/centros_de_formacoes/1/edit') {
 
-        if (!isset($_SERVER['PHP_AUTH_USER'])) {
-            header('WWW-Authenticate: Basic realm="My Website"');
-            header('HTTP/1.0 401 Unauthorized');
-            echo '<p>Access denied. You did not enter a password.</p>';
-            exit; 
+            if (!isset($_SERVER['PHP_AUTH_USER'])) {
+                header('WWW-Authenticate: Basic realm="My Website"');
+                header('HTTP/1.0 401 Unauthorized');
+                echo '<p>Access denied. You did not enter a password.</p>';
+                exit; 
+            }
+
+
+            if ($_SERVER['PHP_AUTH_PW'] == 'ula' && $_SERVER['PHP_AUTH_USER'] == 'ula') {
+                echo '<p>Access granted. You know the password!</p>';
+                return view('centros_de_formacoes.edit',
+                    compact('centros_de_formaco'));
+            } else {
+                echo '<p>Access denied! You do not know the password.</p>';
+                return Redirect::to('/centros_de_formacoes');
+            } 
         }
 
-            
-        if ($_SERVER['PHP_AUTH_PW'] == 'ula' && $_SERVER['PHP_AUTH_USER'] == 'ula') {
-            echo '<p>Access granted. You know the password!</p>';
-        return view('centros_de_formacoes.edit',
-            compact('centros_de_formaco'));
-        } else {
-            echo '<p>Access denied! You do not know the password.</p>';
+        if (url()->current() == 'http://localhost:8080/centros_de_formacoes/2/edit') {
+
+            if (!isset($_SERVER['PHP_AUTH_USER'])) {
+                header('WWW-Authenticate: Basic realm="My Website"');
+                header('HTTP/1.0 401 Unauthorized');
+                echo '<p>Access denied. You did not enter a password.</p>';
+                exit; 
+            }
+
+
+            if ($_SERVER['PHP_AUTH_PW'] == 'ugs' && $_SERVER['PHP_AUTH_USER'] == 'ugs') {
+                echo '<p>Access granted. You know the password!</p>';
+                return view('centros_de_formacoes.edit',
+                    compact('centros_de_formaco'));
+            } else {
+                echo '<p>Access denied! You do not know the password.</p>';
+                return Redirect::to('/centros_de_formacoes');
+            } 
         } 
-      }
-
-            if (url()->current() == 'http://localhost:8080/centros_de_formacoes/2/edit') {
-
-        if (!isset($_SERVER['PHP_AUTH_USER'])) {
-            header('WWW-Authenticate: Basic realm="My Website"');
-            header('HTTP/1.0 401 Unauthorized');
-            echo '<p>Access denied. You did not enter a password.</p>';
-            exit; 
-        }
-
-            
-        if ($_SERVER['PHP_AUTH_PW'] == 'ugs' && $_SERVER['PHP_AUTH_USER'] == 'ugs') {
-            echo '<p>Access granted. You know the password!</p>';
-        return view('centros_de_formacoes.edit',
-            compact('centros_de_formaco'));
-        } else {
-            echo '<p>Access denied! You do not know the password.</p>';
-        } 
-      } 
     }
 
 
     public function update(Request $request, Centro_de_formacao $centro_de_formacao)
-    {
-        //
+    {       //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Centro_de_formacao $centros_de_formaco)
     {
         $centro->delete();
