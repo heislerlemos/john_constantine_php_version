703f3afc516143a5850583e4b2fdd5e867c17b8f
heislerlemos <heisler.stlano@gmail.com>
2024-05-02 13:36:15 +0100

    adicionado os motores de busca

--- resources/views/centros_de_formacoes/layout.blade.php
+++ resources/views/centros_de_formacoes/layout.blade.php
@@ -19,6 +19,45 @@
       <span aria-hidden="true"></span>
     </a>
   </div>
+  <div class="level-left">
+    <div class="level-item">
+      <div class="field has-addons">
+        <p class="control">
+          <input class="input" type="text" placeholder="Encontrar universidade" />
+        </p>
+        <p class="control">
+          <button class="button">Search</button>
+        </p>
+      </div>
+    </div>
+  
+    <div class="level-item">
+<div class="dropdown is-hoverable">
+  <div class="dropdown-trigger">
+    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+      <span>Universidades Disponiveis</span>
+      <span class="icon is-small">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu" role="menu">
+    <div class="dropdown-content">
+      <a href="#" class="dropdown-item"> Dropdown item </a>
+      <a class="dropdown-item"> Other dropdown item </a>
+      <a href="#" class="dropdown-item"> Active dropdown item </a>
+      <a href="#" class="dropdown-item"> Other dropdown item </a>
+      <hr class="dropdown-divider" />
+      <a href="#" class="dropdown-item"> With a divider </a>
+    </div>
+  </div>
+</div>
+
+    </div>
+
+</div>
+
+
 <div class="navbar-end">
       <div class="navbar-item">
         <a class="button is-link  is-outlined" href="{{ route('centros_de_formacoes.create') }}"> Criar novo centro</a>
