7977e44cbea2f3e718862713f2eafa566683ceae
heislerlemos <heisler.stlano@gmail.com>
2024-06-06 16:22:29 +0100

    trabalhando com os 2 formularios

--- resources/views/centros_de_formacoes/edit.blade.php
+++ resources/views/centros_de_formacoes/edit.blade.php
@@ -22,64 +22,6 @@
 
 
 <div class="container">
-<div class="columns is-centered">
-<div class="column ">
-    <form action="{{ route('centros_de_formacoes.update',$centros_de_formaco->id) }}" method="POST" enctype="multipart/form-data" >
-        @csrf
-        @method('PUT')
-
-                <div class="field">
-                    <label class="label">Nome</label>
-                         <div class="control">         
-                            <input class="input is-danger" type="text" name="nome" value="{{ $centros_de_formaco->nome }}">
-                        </div>
-                </div>
-                <div class="field">
-                    <label class="label"> Vagas </label>            
-                    <input class="input is-danger" type="text" name="vagas"  value="{{ $centros_de_formaco->vagas }}">
-                </div>
-
-                    <br>
-                     <button class="button is-danger is-fullwidth is-rounded" type="submit" >Submit</button>
-
-
-
-    </form>
-</div>
-</div>
-
-
-<div class="columns is-mobile is-multiline">
-@foreach($centros_de_formaco->cursos as $curso )
-<div class="column is-half">
-<form action="{{route('centros_de_formacoes.cursos.destroy',$curso->id  )}}" method="post"> 
-        @csrf
-        @method('DELETE')
-
-
-<div class="card ">
-<div class="card-content  has-text-centered ">
-
-
-
-<div class="content">
-<p> Numero de ID {{ $curso->id }} </p>
-</div>
-
-   <footer class="card-footer">
-    <button  type="submit"  class="card-footer-item button is-danger is-rounded ">Apagar</button>
-  </footer>
-
-</div>
-
-
-</div>
-</form>
-</div>
-<!-- end of foreach -->
-@endforeach
-<!-- end of columns -->
-</div>
 <div class="columns is-mobile is-multiline">
 @foreach($centros_de_formaco->cursos as $curso )
 <div class="column is-half">
@@ -123,7 +65,29 @@
 
 
    <footer class="card-footer">
-    <button  type="submit"  class="card-footer-item button is-warning is-rounded ">edit</button>
+    <button  type="submit"  class="card-footer-item button is-warning  ">Editar curso</button>
+
+    
+
+
+  </footer>
+
+</div>
+
+
+</div>
+</form>
+<form action="{{route('centros_de_formacoes.cursos.destroy',$curso->id  )}}" method="post"> 
+        @csrf
+        @method('DELETE')
+
+
+<div class="card ">
+<div class="card-content  has-text-centered ">
+
+
+   <footer class="card-footer">
+    <button  type="submit"  class="card-footer-item button is-danger ">Apagar Curso</button>
   </footer>
 
 </div>
